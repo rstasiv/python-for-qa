ae4eb498139e61d75cadcc30b9cc1a1fab2a46fc
rstasiv <romanstasiv@hotmail.com>
2016-04-28 17:22:03 +0300

    solve task 1

---
+++ Lesson2-1/task1.py
@@ -0,0 +1,9 @@
+
+
+my_string = "All people say hi when the meet each other. Some people are grumpy and do not respond hi but it is always nice to say hi when you have been greeted with hi";
+search = "hi";
+print my_string.count(search)
+
+sentense = "All people say hi when the meet each other. Some people are grumpy and do not respond hi but it is always nice to say hi when you have been greeted with hi";
+print sentense.replace("hi", "bye")
+
